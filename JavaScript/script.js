fd73f5c41695053a537abd54f06f7d5f70650279
nakul9423 <nakulwaghmare9423@gmail.com>
2025-07-03 11:34:01 +0530

    done
    Added New files

---
+++ JavaScript/script.js
@@ -0,0 +1,26 @@
+
+/***************************** Outputs Methods ************************************* */
+//document.write('have a nice Day..!');
+//alert('Hey, how are you..!');
+//console.log('Welcome to JS'); 
+ //document.getElementById('demo').innerHTML = 'Addition of two no is:' + (100 + 100); // js code
+
+
+ /************************** Variable ******************************/
+
+//  1. variable
+//  2. datatypes 
+//  3. prepare statement 
+//  4. operators 
+//  5. loops 
+//  6. functions  
+
+var $first_name="Developer Guru";
+console.log($first_name);
+
+/***
+Rules:
+1. we can use singal data at the same time 
+2. can't allowed space between two word when you are declaring variable name
+3. _,$
+***/
