c4cba9dc0da4872c6549942e11dc0a0022195556
nakul9423 <nakulwaghmare9423@gmail.com>
2025-08-25 19:33:55 +0530

    Added new components

--- ReactJs/ui-328/src/Components/Header/index.js
+++ ReactJs/ui-328/src/Components/Header/index.js
@@ -1,8 +1,13 @@
 import './Header.css';
+import logo from '../../assests/images/dg-logo.jpeg';
 
-const Header=()=>{
+const Header=({name123,name2,name3,name4})=>{
+  console.log(name3);
+  console.log({name4});
   return <>
-    <h1 className='textCss'> THIS IS THE HEADER </h1>
+    <h1 className='textCss'> Welcome to {name123},{name2} </h1>
+    <h5>Wlecome to: {name3[0]}</h5>
+    <img src={logo}></img>
   </>
 
 }
--- ReactJs/ui-328/src/Pages/Home/index.js
+++ ReactJs/ui-328/src/Pages/Home/index.js
@@ -5,9 +5,14 @@ import Footer from '../../Components/Footer/Index';
 
 const Home=()=>{
 
-    return <>
+const name="Developer Guru";
+const name1="Coder";
+const name2=['HTML',"CSS",'Bootstrap','JS'];
+const name3={'H':'HTML','C':'CSS','B':'BOOTSTRAP','R':'REACT JS'}
+
+return <>
     <div>
-        <Header/>
+        <Header name123={name} name2={name1} name3={name2} name4={name3}/>
     </div>
      <div className='bodySection'>
           WELCOME TO HOME 
