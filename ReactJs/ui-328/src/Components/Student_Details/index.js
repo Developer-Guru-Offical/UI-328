c315f72d2c36e9d82845188acac16a77b0c79f0d
nakul9423 <nakulwaghmare9423@gmail.com>
2025-09-12 21:12:06 +0530

    Added new components

--- ReactJs/ui-328/src/Components/Student_Details/index.js
+++ ReactJs/ui-328/src/Components/Student_Details/index.js
@@ -1,13 +1,34 @@
-
-
+import { useEffect, useState } from "react";
+import {studMoreDetails} from '../../Pages/Mockdata';
 
 const Student_Details=()=>{
 
-  const s_id=localStorage.getItem('stud_id');   
+    const[studMoreDetails1,setStudMoreDetails1]=useState(studMoreDetails);
+    const[studDetails,setStudeDetails]=useState({});
+    
+    useEffect(()=>{
+      const stud_data=JSON.parse(localStorage.getItem('stud_id')) || {}; 
+      setStudeDetails(stud_data);
+      //setStudMoreDetails1(studMoreDetails);
+    },[]);
 
  return <>
-   <h1> This is the Student_ Details Page </h1>
-   <p> StudentID:{s_id}</p>
+   <h1> Student Profile Page </h1>
+     <div className='container'>
+      <div className='card mt-4'>
+        <div className='card-body'>
+              <p>Id: {studDetails.id}</p>
+              <p>Name:{studDetails.name} </p>
+              <p>Email:{studDetails.email} </p>
+              <p>Mobile:{studDetails.mob} </p>
+              <p>Gander:{studMoreDetails1[0].Gender} </p>
+               <p>Blood Group:{studMoreDetails1[0].bgroup} </p>
+              <p>Address: {studMoreDetails1[0].address}</p>
+              <p>City: {studMoreDetails1[0].city}</p>
+              <p>Landmark: {studMoreDetails1[0].landmark}</p> 
+       </div>
+        </div>   
+        </div>
  </>
 
 
